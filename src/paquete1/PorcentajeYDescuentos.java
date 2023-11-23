58b6379c27a4c3292d1c1b6d4b42f8b18ca75b34
Administrador <Administrador@2DAM01>
2023-11-23 16:29:43 +0100

    PARTE 1

---
+++ src/paquete1/PorcentajeYDescuentos.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package paquete1;
+
+/**
+ * 
+ */
+public class PorcentajeYDescuentos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
